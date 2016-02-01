880c8dce82f704b44b1a35419e7b04fc6f03ee49
William Springsteen <ws1514@matrix23.doc.ic.ac.uk>
2016-02-01 14:57:07 +0000

    Changed fixed-point functions to macros instead. This is because efficiency is essential here. Left the old fixed-point functions in just in-case the macros do not work or they are not faster.

--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -3,9 +3,25 @@
 
 #include <stdint.h>
 
+/* We are using a P.Q fixed-point format. */
 #define P 17
-#define Q 14
-#define F (1 << 14)
+#define Q (31 - P)
+#define F (1 << Q)
+
+/* In the following macros, x and y denote fixed-point numbers, and
+   n denotes an int. */
+#define TO_FIXED_POINT(n) ((n) * F)
+#define TO_INT_ROUND_ZERO(x) ((x) / F)
+#define TO_INT_ROUND_TO_NEAREST(x) ((x) >= 0 ? ((x) + F / 2) / F   \
+                                             : ((x) - F / 2) / F)
+#define ADD_FIXED_POINTS(x, y) ((x) + (y))
+#define SUB_FIXED_POINTS(x, y) ((x) - (y))
+#define ADD_INT_AND_FIXED_POINT(n, x) ((x) + ((n) * F))
+#define SUB_INT_FROM_FIXED_POINT(x, n) ((x) - ((n) * F))
+#define MUL_FIXED_POINTS(x, y) ((((int64_t) (x)) * (y)) / F)
+#define MUL_INT_AND_FIXED_POINT(x, n) ((x) * (n))
+#define DIV_FIXED_POINTS(x, y) ((((int64_t) (x)) * F) / (y))
+#define DIV_FIXED_POINT_BY_INT(x, n) ((x) / (n))
 
 typedef int32_t fixed_point;
 
