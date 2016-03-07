4dd0f05663102a9ed7509eb0aff8813f597db900
Nacho <ignacio.navarro14@imperial.ac.uk>
2016-03-07 15:57:56 +0000

    Using a bitmap, since the spec says bitmaps are best when the data tracked is identical (in this case pages).

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -2,19 +2,23 @@
 #include "devices/block.h"
 
 struct block *swap_block;
+static struct bitmap *swap_bitmap;
+static int pages_in_swap_space;
 
 void
 swap_init()
 {
     swap_block = block_get_role(BLOCK_SWAP);
+    pages_in_swap_space = block_size(swap_block) / SECTORS_PER_PAGE;
+    swap_bitmap = bitmap_create(pages_in_swap_space);
 }
 
 void
 swap_in(void *page_start)
 {
-    int sectors_in_block = swap_block->size;
+
     int i;
-    for (i = 0; i < sectors_in_block; i++)
+    for (i = 0; i < SECTORS_PER_PAGE; i++)
         {
             block_write(swap_block, i, page_start + i);
         }
@@ -23,9 +27,8 @@ swap_in(void *page_start)
 void
 swap_out(void *page_start)
 {
-    int sectors_in_block = swap_block->size;
     int i;
-    for (i = 0; i < sectors_in_block; i++)
+    for (i = 0; i < SECTORS_PER_PAGE; i++)
         {
             block_read(swap_block, i, page_start + i);
         }
--- src/vm/swap.h
+++ src/vm/swap.h
@@ -1,6 +1,11 @@
 #ifndef VM_SWAP_H
 #define VM_SWAP_H
 
+#include "threads/vaddr.h"
+#include "devices/block.h"
+
+#define SECTORS_PER_PAGE (PGSIZE / BLOCK_SECTOR_SIZE)
+
 void swap_out(void *);
 void swap_int(void *);
 
