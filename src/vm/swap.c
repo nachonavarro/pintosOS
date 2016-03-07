5f73e5713bbe49233774a1bfed6c85887a55cd42
Nacho <ignacio.navarro14@imperial.ac.uk>
2016-03-07 14:53:58 +0000

    Wrote swap_in and swap_out functions for a single page, still don't know which data structure I'm going to use for the swap table or when to use the swap table.

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -12,11 +12,21 @@ swap_init()
 void
 swap_in(void *page_start)
 {
-
+    int sectors_in_block = swap_block->size;
+    int i;
+    for (i = 0; i < sectors_in_block; i++)
+        {
+            block_write(swap_block, i, page_start + i);
+        }
 }
 
 void
 swap_out(void *page_start)
 {
-
+    int sectors_in_block = swap_block->size;
+    int i;
+    for (i = 0; i < sectors_in_block; i++)
+        {
+            block_read(swap_block, i, page_start + i);
+        }
 }
