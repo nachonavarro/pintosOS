e37c6305e2aa815bad47d3ba811323624d32c989
Nacho <ignacio.navarro14@imperial.ac.uk>
2016-03-07 14:13:08 +0000

    Added template.

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -1,1 +1,22 @@
 #include "vm/swap.h"
+#include "devices/block.h"
+
+struct block *swap_block;
+
+void
+swap_init()
+{
+    swap_block = block_get_role(BLOCK_SWAP);
+}
+
+void
+swap_in(void *page_start)
+{
+
+}
+
+void
+swap_out(void *page_start)
+{
+
+}
--- src/vm/swap.h
+++ src/vm/swap.h
@@ -1,5 +1,7 @@
 #ifndef VM_SWAP_H
 #define VM_SWAP_H
 
+void swap_out(void *);
+void swap_int(void *);
 
 #endif /* vm/swap.h */
