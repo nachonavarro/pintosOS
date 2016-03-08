7e888606352be6d90e7ca4f2aedc83a079df3633
Nacho <ignacio.navarro14@imperial.ac.uk>
2016-03-08 13:41:24 +0000

    Added a TODO in swap_out, I still don't know how we can access the slot to swap out.

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -1,35 +1,47 @@
 #include "vm/swap.h"
 #include "devices/block.h"
 
-struct block *swap_block;
+struct block *swap_space;
 static struct bitmap *swap_bitmap;
 static int pages_in_swap_space;
 
 void
 swap_init()
 {
-    swap_block = block_get_role(BLOCK_SWAP);
-    pages_in_swap_space = block_size(swap_block) / SECTORS_PER_PAGE;
+    swap_space = block_get_role(BLOCK_SWAP);
+    pages_in_swap_space = block_size(swap_space) / SECTORS_PER_PAGE;
     swap_bitmap = bitmap_create(pages_in_swap_space);
 }
 
 void
 swap_in(void *page_start)
 {
-
+    /* Find a free slot and set it to occupied.*/
+    int free_slot_index =
+            bitmap_scan_and_flip(swap_bitmap, BITMAP_START_INDEX,
+                                    NUM_OF_SLOTS_TO_SWAP, false);
     int i;
     for (i = 0; i < SECTORS_PER_PAGE; i++)
         {
-            block_write(swap_block, i, page_start + i);
+            block_write(swap_space, SECTORS_PER_PAGE * free_slot_index + i,
+                                                            page_start + i);
         }
 }
 
 void
 swap_out(void *page_start)
 {
+
+    // TODO: I still need to figure out how to access the right slot in the bitmap.
+    // For swapping in it isn't a problem, as we can find any free slot.
+    // For swapping out though, the slot is fixed to the slot corresponding to the page.
+    int free_slot_index =
+                bitmap_scan_and_flip(swap_bitmap, BITMAP_START_INDEX,
+                                        NUM_OF_SLOTS_TO_SWAP, true);
     int i;
     for (i = 0; i < SECTORS_PER_PAGE; i++)
         {
-            block_read(swap_block, i, page_start + i);
+            block_read(swap_space, i, SECTORS_PER_PAGE * free_slot_index + i,
+                                                              page_start + i);
         }
 }
--- src/vm/swap.h
+++ src/vm/swap.h
@@ -5,6 +5,9 @@
 #include "devices/block.h"
 
 #define SECTORS_PER_PAGE (PGSIZE / BLOCK_SECTOR_SIZE)
+#define BITMAP_START_INDEX 0
+#define NUM_OF_SLOTS_TO_SWAP 1
+
 
 void swap_out(void *);
 void swap_int(void *);
