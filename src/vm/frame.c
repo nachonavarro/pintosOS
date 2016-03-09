c4b6a46b30e8d438502dc1fc4eee3e81afff5aa5
Billy Springsteen <william.springsteen14@imperial.ac.uk>
2016-03-09 21:07:56 +0000

    Think I have pretty much finished implementing frame table without eviction - Still need to tweak and check a few bits, and change where palloc_get_page and palloc_free_page are called (in process.c only?).

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -2,68 +2,134 @@
 #include "lib/random.h"
 
 static struct list frame_table;
+static struct lock frame_table_lock;
 
-void frame_table_init(void) {
+/* Initialise the actual frame table itself, along with any locks required in
+   accessing the frame table. */
+void
+frame_table_init(void) {
   list_init(&frame_table);
-  //lock_init...
+  lock_acquire(&frame_table_lock);
 }
 
-bool add_frame(void *page) {
-  /* Frames used for user pages should be obtained from the user pool. */
-  struct frame f = malloc(sizeof(struct frame)); //Should this be malloc(sizeof(struct frame))? And page should be made using palloc??
-  if (f == NULL) {
+//TODO: Make static?
+/* Creates a frame that will contain a pointer to the given page, and adds
+   this frame to the frame table. Returns true if frame was successfully
+   added, or false otherwise (i.e. if there was not enough memory to
+   malloc space for a struct frame). Called in alloc_frame(). */
+bool
+add_frame(void *frame, void *upage) {
+  /* Frame is freed in remove_frame(). */
+  struct fte *fte = malloc(sizeof(struct fte));
+
+  /* Return false if struct fte could not be successfully malloc'd. */
+  if (fte == NULL) {
     return false;
   }
-  f->page = page;
-  f->owner = (pid_t)thread_current()->tid;
-  //NEED LOCK HERE, AS LIST IS 'GLOBAL'
-  list_push_back(&frame_table, &f->frame_elem);
+
+  /* Set members of struct fte. */
+  fte->frame = frame;
+  fte->upage = upage;
+  fte->owner = (pid_t)thread_current()->tid;
+
+  /* Add the created frame to the frame table. Must acquire a lock while
+     accessing this list, because other threads could try to access this list
+     at the same time. */
+  lock_acquire(&frame_table_lock);
+  list_push_back(&frame_table, &fte->fte_elem);
+  lock_release(&frame_table_lock);
+
+  /* Return true as the frame must have been successfully created to get to
+     this line of code (as false would have been returned otherwise). */
   return true;
 }
 
-void remove_frame(void *page) {
+//TODO: Make static?
+/* Removes the frame from the frame table that has the pointer to the
+   supplied page in it. Called in free_frame(). */
+void
+remove_frame(void *frame) {
   struct list_elem *e;
-  struct frame *f;
-  //lock_acquire
-  for(e = list_begin(&frame_table); e != list_end(&frame_table); e = list_next(e)) {
-    f = list_entry(e, struct frame, frame_elem);
-    if (f->page == page) {
+  struct fte *fte;
+
+  /* Traverse frame table until we find a frame with a pointer to the
+     supplied page. */
+  lock_acquire(&frame_table_lock);
+  for(e = list_begin(&frame_table);
+      e != list_end(&frame_table);
+      e = list_next(e)) {
+
+    fte = list_entry(e, struct fte, fte_elem);
+
+    if (fte->frame == frame) {
       list_remove(e);
-      free(f); //Change to free if we malloc frames instead of use get_page
-      //Do we need to free the page as well??
+      /* Ensure we free the fte, as we malloc'd space for this in
+         add_frame(). */
+      free(fte);
       break;
     }
+
   }
-  //lock_release
+  lock_release(&frame_table_lock);
 }
 
-//Called instead of palloc_get_page(). Adds the page to an entry in the frame
-//table. Eviction will happen if frame table is full. Returns the same thing
-//that palloc_get_page() would return, although takes eviction into account.
 //TODO: Change calls to palloc_get_page to alloc_frame (in process.c only??)
-void *alloc_frame(void) {
-  void *page = palloc_get_page(PAL_USER);
-  if (page == NULL) {
-    //TODO: Frame table is full(?) -> Evict!
-    //PANIC if no frame can  be evicted without allocating a swap slot, and swap slot is full
-    //If eviction successful, must set page to the correct thing now, so we can return it
+//      where it will be set to a new member in the struct fte?
+//      I think we do, but not sure why we need this upage...
+//      (This upage may just be the pointer to the user page, if any, that
+//      occupies it)
+/* Called instead of palloc_get_page(). Gets a page and then adds a frame to
+   the frame table that points to that page. Calls a function to handle
+   eviction if the frame table is full. Returns the page returned from
+   palloc_get_page(), or the return value of evict(). Panics if no frame
+   can evicted without allocating a swap slot, and swap slot is full. */
+void *
+alloc_frame(void *upage) {
+  /* A frame is just a page sized region of physical memory, accessed through
+     kernel virtual memory (returned from palloc_get_page()) */
+  void *frame = palloc_get_page(PAL_USER);
+
+  /* If frame table is full, we need to evict a frame, and return the evicted
+     frame (where old contents have been evicted and new contents have been
+     put in the evicted frame). */
+  if (frame == NULL) {
+    frame = evict(upage);
+    //TODO: PANIC if no frame can  be evicted without allocating a swap slot,
+    //      and swap slot is full (I think evict will return null in this case)
   } else {
-    add_frame(page); //Otherwise, add to the frame table
+    add_frame(frame, upage); //Otherwise, add to the frame table
   }
-  return page;
+  return frame;
+}
+
+/* Remove frame for this page from frame table, and then free the page
+   itself. Called instead of palloc_free_page() (in process.c only??).
+   Argument is return value of alloc_frame(). */
+void
+free_frame(void *frame) {
+  remove_frame(frame);
+  palloc_free_page(frame);
 }
 
 /* Called if frame table is full. Currently chooses a RANDOM frame in
    frame_table. */
-struct frame *choose_frame_to_evict(void) {
-  int frames = list_size(&frame_table); //Need to make sure we don't evict frames we have 'removed' if they stay in the list
-  ASSERT(frames > 0); //Should call when table is full (Don't know max size right now)
-  int random_frame = ((int) random_ulong()) % (frames); //Random number between 0 and (size - 1) inclusive.
+struct fte *
+choose_frame_to_evict(void) {
+  int ftes = list_size(&frame_table); //Need to make sure we don't evict frames we have 'removed' if they stay in the list
+  ASSERT(ftes > 0); //Should call when table is full (Don't know max size right now)
+  int random_fte = ((int) random_ulong()) % (ftes); //Random number between 0 and (size - 1) inclusive.
   struct list_elem *e;
-  for (e = list_begin(&frame_table); random_frame > 0; random_frame--) {
+  for (e = list_begin(&frame_table); random_fte > 0; random_fte--) {
     e = list_next(e);
   }
-  struct frame *frame = list_entry(e, struct frame, frame_elem);
-  return frame;
+  struct fte *fte = list_entry(e, struct fte, fte_elem);
+  return fte;
 }
 
+/* Returns frame, like alloc_frame() would have returned. Returns
+   NULL on failure. */
+void *
+evict(void *upage) {
+  //TODO: Calls choose_frame_to_evict() first?
+  return NULL;
+}
--- src/vm/frame.h
+++ src/vm/frame.h
@@ -1,14 +1,23 @@
 #ifndef VM_FRAME_H
 #define VM_FRAME_H
 
-struct frame {
-  void *page; /* Pointer to page that currently occupies this frame. */
-  pid_t owner; /* pid of process that owns this frame. */
+//TODO: Do we need a paddr and vaddr OR upage and vpage? Is this just frame
+//      and upage?...
+struct fte {
+  void *frame; /* The frame itself, as the frame is 'just a page'. */
+  void *upage; /* Pointer to page that currently occupies this frame. */
 
-  struct list_elem frame_elem; /* To allow each frame to be added to 'static
+  //Page table will have mapping from upage (user virtual address) to frame
+  //(kpage - kernel virtual address - how we access frame as frame is region
+  //of contiguous physical memory and there is a mapping from physical memory
+  //to kernel virtual memory [identity??])
+
+  pid_t owner; /* pid of process that owns this frame. */
+  struct list_elem fte_elem; /* To allow each frame to be added to 'static
                                   struct list frames' in 'frame.c'. */
 };
 
+//TODO: Put functions in header - not static ones.
 /*
   bool add_frame(void *frame, void *uaddr); //Add frame to table
   void remove_frame(void *frame); //Remove frame from table (and call
