e0515521ff4c5e416b5c9ae7016128c26ede1d29
Billy Springsteen <william.springsteen14@imperial.ac.uk>
2016-03-18 10:43:52 +0000

    Only things left for mmap are: calling munmap when process exits, spt_insert & spt_remove & spt to check if page is already used, copy-on-write(???), lazy loading (???). Compiles.

--- src/vm/mmap.h
+++ src/vm/mmap.h
@@ -9,11 +9,15 @@ struct mmap_mapping {
   mapid_t mapid; /* Uniquely identifies the mapping (within the process). The
                     hash table mmap_table will take mapid as a key, and the
                     struct mmap_mapping it is in will be the value. */
-  int size; /* Size of file in bytes. If size is not a multiple of PGSIZE,
-               some bytes in the final mapped page 'stick out' beyond end of
-               file. */
+//  int size; /* Size of file in bytes. If size is not a multiple of PGSIZE,
+//               some bytes in the final mapped page 'stick out' beyond end of
+//               file. */
+  int num_pages; /* Number of pages that this file will take up when mapped.
+                    If end file is just 1 byte into a page, that whole page is
+                    needed. */
   //TODO: Not sure whether to have size or end_uaddr
-  void *uaddr; /* Start address that file is mapped to. */
+  void *start_uaddr; /* Start address that file is mapped to. */
+  void *end_uaddr; /* End address that file is mapped to. */
   //TODO: For eviction, having a file descriptor here may be better?
   //      Maybe we need both
   struct file *file; /* File that is mapped. Not the same struct as in
@@ -21,9 +25,11 @@ struct mmap_mapping {
                         file_reopen() is used. */
 };
 
-bool mmap_table_insert(struct hash *mmap_table, void *uaddr, int size, mapid_t mapid, struct file* file);
-struct mmap_mapping *mmap_mapping_lookup(struct hash *mmap_table, const mapid_t mapid);
-void mmap_mapping_delete(struct hash *mmap_table, const mapid_t mapid);
+bool mmap_table_insert(struct hash *mmap_table, void *start_uaddr,
+    void *end_uaddr, int num_pages, mapid_t mapid, struct file* file);
+struct mmap_mapping *mmap_mapping_lookup(struct hash *mmap_table,
+                                                    const mapid_t mapid);
+void mmap_mapping_delete(struct hash *mmap_table, struct mmap_mapping *mmap);
 unsigned mapid_hash(const struct hash_elem *, void *);
 bool mapid_less(const struct hash_elem *, const struct hash_elem *, void *);
 void destroy_mmap_table(struct hash *mmap_table);
